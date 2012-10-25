705ad1276e6b71f251539397423e0f9949bed83c
aklump <sourcecode@intheloftstudios.com>
2012-10-25 08:17:21 -0700

    imported 7.x branch

--- README.txt
+++ README.txt
@@ -10,42 +10,10 @@ REQUIREMENTS:
 INSTALLATION:
 - Download and unzip this module into your modules directory.
 - Goto Administer > Site Building > Modules and enable this module.
-- Go to http://flowplayer.org/download/ and download into
-  /sites/all/libraries/flowplayer/
-
-- Consider downloading the flowplayer.ipad plugin from:
-  http://flowplayer.org/plugins/javascript/ipad.html.
-- Download and rename to:
-  /sites/all/libraries/flowplayer_ipad/flowplayer.ipad.js
 
 
 CONFIGURATION:
-- You can install the getid3 module and it will work with this module and
-  provide duration information
-- If you want duration information avabilable in your node tpl then you should
-  enable filefield_meta and http://drupal.org/project/getid3 modules
-
-
-SUPPORTED PLAYER TYPES:
-- flowplayer: standard flowplayer; last tested on version 3.2.8
-- flowplayer_ipad: includes the flowplayer ipad plugin found here:
-  http://flowplayer.org/plugins/javascript/ipad.html
-
-
-VIDEO ENCODING
-- (excerpt from http://flowplayer.org/plugins/javascript/ipad.html)
-- As iOS does not support Flash, videos in Flash format - FLV, F4V etc. - cannot
-  be played on iDevices.
-- Videos deployed on iDevices must be encoded in the MPEG4 Part 10 format using
-  the h.264 video codec. Within the MPEG4 specs make sure that your videos meet
-  the constraints of the devices of your target audience. Older and small
-  (iPhone, iPod) iDevices allow only lower h.264 profiles and levels because
-  higher levels and profiles are expensive to decode. To achieve maximum
-  backwards compatibility with early iPhones and iPods you must use the
-  Constrained Baseline Profile (CBP) at maximum Level 3.0, and the video's
-  dimensions must not exceed 640x480 pixels. Moreover, you should not permit
-  more than 2 reference frames to avoid the risk of square artifacts during
-  playback.
+- 
 
 
 USAGE:
@@ -55,20 +23,6 @@ USAGE:
 - If you utilize the latter, your embed code will be available in node.tpl.php
   as a variable called {$fieldname}_embed.
 
-
-EXAMPLE USAGE:
-- Add a filefield to a node type OR create a video node type (@see
-  sample_code/video_type.cck.php)
-- Register that field from within a custom module using hook_loft_video()
-- You will have access to ta variable called {$fieldname}_embed to be used in
-  your node tpl for placement of the video.
-- You can also include it in the #content var; @see loft_video.api.php
-
-
-VIEWS:
-- A view is provided which lists all videos so you can easily test them on iOs
-  devices. View is located here: /admin/content/loft-video/list and is limited
-  to permissions: administer content
 --------------------------------------------------------
 CONTACT:
 In the Loft Studios
--- loft_video.api.php
+++ loft_video.api.php
@@ -3,113 +3,25 @@
 
 /**
  * @file
- * API documentation for loft_video module.
+ * API documentation for loft_video module. *
  */
 
 /**
  * Implementation of hook_loft_video
-
+ 
  * @return array
  *   An associate array with keys who are the node_types that contain video files
  *   Each key is an array of fieldnames, within that node type, that contain video files
- *   Here are the keys within each fieldname
- *
- *   It is advised to apply height and width using external stylesheets, however
-     you may apply height and width here which is rendered in the style
-     attribute.
- *
- *   - width: int (optional. Will be applied as a style attribute and will override anything else)
- *   - height: int (optional. Will be applied as a style attribute and will override anything else)
- *   - type: currently only 'flowplayer'
- *   - autoplay: bool; defaults to TRUE
- *   - content_render: should the rendered value of the field be replaced with the
-     embed code in hook_preprocess_node in $vars['content']?
-     - config: This will be player dependent and allows you send player
-       configuration options to the player
-       - flowplayer config: //@see http://flowplayer.org/documentation/configuration/
+ *   Each filename is an array with attributes: width, height
  */
 function hook_loft_video() {
   return array(
     'video' => array(
       'field_video_file' => array(
-        'type' => 'flowplayer_ipad',
-        'autoplay' => FALSE,
-        'content_render' => TRUE,
-        //'width' => '400', //width in pixels
-        //'height' => '300', //width in pixels
-        //'config' => array(
-        //  'clip' => array(
-        //    'autoPlay' => FALSE,
-        //  ),
-        //),
+        'width' => '746', //width in pixels
+        'height' => '420', //width in pixels
+        'type' => 'flowplayer',
       ),
     ),
   );
-}
-
-/**
- * Implements hook_loft_video_player_source_alter().
- *
- * Allows modules to alter the source directory of a player by type
- *
- * @param string $path
- *   The path as determined by type
- * @param string $type
- *   The type of video player
- *
- * @return NULL
- */
-function hook_loft_video_player_source_alter(&$path, $type) {
-  if ($type = 'my_custom_player') {
-    $path = 'sites/all/libraries/my_custom_player';
-  }
-}
-
-/**
- * Implements hook_loft_video_player_version_alter().
- *
- * Allows modules to alter the player version by type
- *
- * @param string $version
- *   The version as determined by type
- * @param string $type
- *   The type of video player
- *
- * @return NULL
- */
-function hook_loft_video_player_version_alter(&$version, $type) {
-  if ($type = 'my_custom_player') {
-    $version = '1.0';
-  }
-}
-
-/**
- * Alter the output of theme_loft_video_player
- *
- * @param string $output
- *   The html markup coming out of the theme function
- * @param array $vars
- *   The $vars array coming out of the function
- * @param array $original_vars
- *   The $vars array going in to the function
- *
- * @return NULL
- */
-function hook_loft_video_player_alter(&$output, $vars, $original_vars) {
-
-}
-
-/**
- *
- * Implements hook_loft_video_file_urls_alter().
- *
- * Allow modules to alter the video file urls for a node
- *
- * @param array $video_urls
- * @param object $node
- *
- * @return NULL
- */
-function hook_loft_video_file_urls_alter(&$video_urls, $node) {
-
 }
