91eb83d37e4e35d37946efb34f30a706fc3900c8
aklump <sourcecode@intheloftstudios.com>
2012-11-03 18:51:19 -0700

    Added support for auto_content

--- loft_video.api.php
+++ loft_video.api.php
@@ -8,15 +8,19 @@
 
 /**
  * Implementation of hook_loft_video
- 
+
  * @return array
  *   An associate array with keys who are the node_types that contain video files
+ *   Properties of each array are:
+ *   - #auto_content: set to True and the field will be rendered as a video,
+     overriding the current display handler
  *   Each key is an array of fieldnames, within that node type, that contain video files
  *   Each filename is an array with attributes: width, height
  */
 function hook_loft_video() {
   return array(
     'video' => array(
+      '#auto_content' => TRUE,
       'field_video_file' => array(
         'width' => '746', //width in pixels
         'height' => '420', //width in pixels
